eea967d90d5f84ad4561cd8e57b2276fb83dc806
Adam <arbimer@mac.com>
2022-01-31 14:37:02 -0600

    Fixes for task 1 and updated files for task 2

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,23 +1,53 @@
-const graphQL = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphQL;
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLInt,
+} = require('graphql');
+const lodash = require('lodash');
 
-const RootQuery = GraphQLObjectType({
-  name: 'RootQueryType',
-  fields: {
-    type: { type: TaskType },
-    args: { id: { type: GraphQLString } },
-    resolve: (parent, args) => {},
+const data = [
+  {
+    id: '1',
+    title: 'Create your first webpage',
+    weight: 1,
+    description:
+      'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+    projectId: 1,
   },
-});
+  {
+    id: '2',
+    title: 'Structure your webpage',
+    weight: 1,
+    description:
+      'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+    projectId: 1,
+  },
+];
 
-const TaskType = GraphQLObjectType({
+const TaskType = new GraphQLObjectType({
   name: 'Task',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     title: { type: GraphQLString },
     weight: { type: GraphQLInt },
     description: { type: GraphQLString },
-  },
+  }),
 });
 
-module.exports = { RootQuery, GraphQLObjectType };
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: () => ({
+    task: {
+      type: TaskType,
+      args: { id: { type: GraphQLString } },
+      resolve: (parent, args) => lodash.find(data, { id: args.id }),
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: RootQuery,
+});
+
+module.exports = schema;
