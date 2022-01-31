7b17e8d154133b88d523459568541f0bbe637f6a
Adam <arbimer@mac.com>
2022-01-31 14:18:29 -0600

    Updates for task 1

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./scheam/schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({}));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
 app.listen(4000, () => {
   console.log('now listening for request on port 4000');
 });
--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,6 +1,15 @@
 const graphQL = require('graphql');
 const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphQL;
 
+const RootQuery = GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    type: { type: TaskType },
+    args: { id: { type: GraphQLString } },
+    resolve: (parent, args) => {},
+  },
+});
+
 const TaskType = GraphQLObjectType({
   name: 'Task',
   fields: {
@@ -11,4 +20,4 @@ const TaskType = GraphQLObjectType({
   },
 });
 
-module.exports = { TaskType };
+module.exports = { RootQuery, GraphQLObjectType };
