673344bc991023cff9e6f53b71df5a883eca7f3b
Adam <arbimer@mac.com>
2022-01-31 16:03:21 -0600

    Removed fake data arrays from server/server.js and pointed resolve functions to mongo

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -11,43 +11,6 @@ const lodash = require('lodash');
 const Project = require('../models/project');
 const Task = require('../models/task');
 
-// fake data
-const tasks_data = [
-  {
-    id: '1',
-    title: 'Create your first webpage',
-    weight: 1,
-    description:
-      'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
-    projectId: '1',
-  },
-  {
-    id: '2',
-    title: 'Structure your webpage',
-    weight: 1,
-    description:
-      'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
-    projectId: '1',
-  },
-];
-
-const projects_data = [
-  {
-    id: '1',
-    title: 'Advanced HTML',
-    weight: 1,
-    description:
-      "Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don't worry, the final page will be “ugly” it's normal, it's not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!",
-  },
-  {
-    id: '2',
-    title: 'Bootstrap',
-    weight: 1,
-    description:
-      'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.',
-  },
-];
-
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: () => ({
@@ -57,8 +20,7 @@ const TaskType = new GraphQLObjectType({
     description: { type: GraphQLString },
     project: {
       type: TaskType,
-      resolve: (parent, args) =>
-        lodash.find(projects_data, { id: parent.projectId }),
+      resolve: (parent, args) => Project.findById(parent.projectId),
     },
   }),
 });
@@ -72,8 +34,7 @@ const ProjectType = new GraphQLObjectType({
     description: { type: GraphQLString },
     tasks: {
       type: new GraphQLList(TaskType),
-      resolve: (parent, args) =>
-        lodash.filter(tasks_data, { projectId: parent.id }),
+      resolve: (parent, args) => Task.find({ projectId: parent.id }),
     },
   }),
 });
@@ -117,20 +78,20 @@ const RootQuery = new GraphQLObjectType({
     task: {
       type: TaskType,
       args: { id: { type: GraphQLString } },
-      resolve: (parent, args) => lodash.find(tasks_data, { id: args.id }),
+      resolve: (parent, args) => Task.findById(args.id),
     },
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLString } },
-      resolve: (parent, args) => lodash.find(projects_data, { id: args.id }),
+      resolve: (parent, args) => Project.findById(args.id),
     },
     tasks: {
       type: new GraphQLList(TaskType),
-      resolve: () => tasks_data,
+      resolve: () => Task.find({}),
     },
     projects: {
       type: new GraphQLList(ProjectType),
-      resolve: () => projects_data,
+      resolve: () => Project.find({}),
     },
   }),
 });
