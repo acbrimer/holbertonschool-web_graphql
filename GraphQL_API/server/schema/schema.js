32bfe2d7d68448a515d512f3de0955fdcd8ce48c
Adam <arbimer@mac.com>
2022-01-31 14:42:35 -0600

    Added solution for task 3

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -3,10 +3,11 @@ const {
   GraphQLSchema,
   GraphQLString,
   GraphQLInt,
+  GraphQLID,
 } = require('graphql');
 const lodash = require('lodash');
 
-const data = [
+const tasks_data = [
   {
     id: '1',
     title: 'Create your first webpage',
@@ -25,10 +26,37 @@ const data = [
   },
 ];
 
+const projects_data = [
+  {
+    id: '1',
+    title: 'Advanced HTML',
+    weight: 1,
+    description:
+      "Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don't worry, the final page will be “ugly” it's normal, it's not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!",
+  },
+  {
+    id: '2',
+    title: 'Bootstrap',
+    weight: 1,
+    description:
+      'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.',
+  },
+];
+
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: () => ({
     id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    weight: { type: GraphQLInt },
+    description: { type: GraphQLString },
+  }),
+});
+
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
     title: { type: GraphQLString },
     weight: { type: GraphQLInt },
     description: { type: GraphQLString },
@@ -41,7 +69,12 @@ const RootQuery = new GraphQLObjectType({
     task: {
       type: TaskType,
       args: { id: { type: GraphQLString } },
-      resolve: (parent, args) => lodash.find(data, { id: args.id }),
+      resolve: (parent, args) => lodash.find(tasks, { id: args.id }),
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLString } },
+      resolve: (parent, args) => lodash.find(projects, { id: args.id }),
     },
   }),
 });
